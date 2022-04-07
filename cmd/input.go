c04a0f62ca868f366f6331af19e59b4706b2947d
edoardottt <edoardott@gmail.com>
2022-04-07 16:42:36 +0200

    First template structure tentative

--- cmd/input.go
+++ cmd/input.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v3"
+)
+
+//ReadTemplate gets as input a filename and returns a Template object.
+func ReadTemplate(filename string) (Template, error) {
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return Template{}, err
+	}
+
+	c := Template{}
+	err = yaml.Unmarshal(buf, &c)
+	if err != nil {
+		return Template{}, fmt.Errorf("in file %q: %v", filename, err)
+	}
+
+	return c, nil
+}
---
+++ cmd/template.go
@@ -0,0 +1,49 @@
+package main
+
+//TemplateType contains all the types that a template can assume
+type TemplateType string
+
+const (
+	RawTemplateType    TemplateType = "raw"
+	ShodanTemplateType TemplateType = "shodan"
+)
+
+//HttpMethod contains all the methods that a HTTP request can assume
+type HttpMethod string
+
+const (
+	MethodGet     HttpMethod = "GET"
+	MethodHead    HttpMethod = "HEAD"
+	MethodPost    HttpMethod = "POST"
+	MethodPut     HttpMethod = "PUT"
+	MethodPatch   HttpMethod = "PATCH" // RFC 5789
+	MethodDelete  HttpMethod = "DELETE"
+	MethodConnect HttpMethod = "CONNECT"
+	MethodOptions HttpMethod = "OPTIONS"
+	MethodTrace   HttpMethod = "TRACE"
+)
+
+//ResponseType contains all the types that a HTTP response can assume
+type ResponseType string
+
+const (
+	RawResponseType  ResponseType = "raw"
+	FileResponseType ResponseType = "file"
+)
+
+//Request is the struct defining an HTTP request
+type Request struct {
+	Id           string       `yaml:"id,omitempty"`
+	Method       HttpMethod   `yaml:"method,omitempty"`
+	Endpoint     string       `yaml:"endpoint,omitempty"`
+	ResponseType ResponseType `yaml:"response-type,omitempty"`
+	ContentType  string       `yaml:"content-type,omitempty"`
+	Content      string       `yaml:"content,omitempty"`
+}
+
+//Template is the struct defining the structure of a configuration template
+type Template struct {
+	Type     TemplateType `yaml:"type,omitempty"`
+	Requests []Request    `yaml:"requests,omitempty"`
+	Ip       string       `yaml:"ip,omitempty"`
+}
