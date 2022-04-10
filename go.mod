904a6fcc7220593ed9e805eee3d39d4666e93221
edoardottt <edoardott@gmail.com>
2022-04-10 18:19:38 +0200

    StackOverflow saved me!

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
--- server/honeypot/honeypot.go
+++ server/honeypot/honeypot.go
@@ -2,62 +2,40 @@ package honeypot
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/edoardottt/boggart/pkg/template"
+	"github.com/gorilla/mux"
 )
 
 func genericWriter(w http.ResponseWriter, req *http.Request, response string) {
 	fmt.Fprint(w, response)
 }
 
-func defaultRootWriter(w http.ResponseWriter, req *http.Request, response string, defaultResponse string) {
-	fmt.Println(">" + req.URL.Path + "<")
-	if req.URL.Path != "/" {
-		fmt.Println(req.URL.Path)
-		fmt.Fprint(w, defaultResponse)
-	} else {
-		fmt.Println(response)
-		fmt.Fprint(w, response)
-	}
-}
-
 //RawHoneypot
 func RawHoneypot(tmpl template.Template) {
 
-	//This variable says if the root endpoint ("/")
-	//is defined through the template.
-	var rootExists = template.RootEndpointExists(tmpl)
+	r := mux.NewRouter()
 
-	//IF A REQUEST WITH ENDPOINT == "/" EXISTS:
-	if rootExists {
-		for _, request := range tmpl.Requests {
-			if request.Endpoint != "" && request.Endpoint != "/" {
-				http.HandleFunc(request.Endpoint, func(w http.ResponseWriter, r *http.Request) {
-					genericWriter(w, r, request.Content)
-				})
-			} else {
-				//HANDLE DEFAULT THERE
-				if request.Id != "default" {
-					fmt.Println(request.Id + " " + request.Content)
-					http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-						defaultRootWriter(w, r, request.Content, template.DefaultResponse(tmpl))
-					})
-				}
-			}
-		}
-	} else {
-		//OTHERWISE
-		for _, request := range tmpl.Requests {
-			http.HandleFunc(request.Endpoint, func(w http.ResponseWriter, r *http.Request) {
-				genericWriter(w, r, request.Content)
-			})
-		}
-		//DEFINE "/" WITH DEFAULT RESPONSE
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			genericWriter(w, r, template.DefaultResponse(tmpl))
+	for _, request := range tmpl.Requests {
+		request2 := request
+		r.HandleFunc(request2.Endpoint, func(w http.ResponseWriter, r *http.Request) {
+			genericWriter(w, r, request2.Content)
 		})
 	}
+	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, template.DefaultResponse(tmpl))
+	})
 
-	http.ListenAndServe(":8090", nil)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8090",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
