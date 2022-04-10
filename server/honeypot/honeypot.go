ae01ff44a038c887ecac905736694341428830c6
edoardottt <edoardott@gmail.com>
2022-04-10 15:23:42 +0200

    Init honeypot

--- server/honeypot/honeypot.go
+++ server/honeypot/honeypot.go
@@ -1,1 +1,19 @@
 package honeypot
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func genericWriter(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello\n")
+}
+
+//Honeypot
+func Honeypot() {
+
+	http.HandleFunc("/", genericWriter)
+
+	http.ListenAndServe(":8090", nil)
+}
