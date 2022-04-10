27d2539d5ec29a8442e7e6d0272ddfa2f73bd586
edoardottt <edoardott@gmail.com>
2022-04-10 11:23:29 +0200

    Add mongoDB

--- db/connect.go
+++ db/connect.go
@@ -1,1 +1,33 @@
 package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//ConnectDB creates and returns a client connected by a
+//connection string to mongoDB.
+//Also checks the connection if everything is ok.
+func ConnectDB(connectionString string) (*mongo.Client, context.Context) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!") //!! DEBUG !!
+	return client, ctx
+}
