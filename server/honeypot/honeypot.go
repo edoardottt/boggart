f958b21073bdf5fb87dd5ecebaa0b07429bf067b
edoardottt <edoardott@gmail.com>
2022-04-11 15:30:26 +0200

    File server + content type

--- server/honeypot/honeypot.go
+++ server/honeypot/honeypot.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/edoardottt/boggart/internal/file"
 	"github.com/edoardottt/boggart/pkg/template"
 	"github.com/gorilla/mux"
 )
@@ -14,22 +15,57 @@ func genericWriter(w http.ResponseWriter, req *http.Request, response string) {
 	fmt.Fprint(w, response)
 }
 
+func fileWriter(w http.ResponseWriter, req *http.Request, inputFile string) {
+	content, err := file.ReadFile(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprint(w, content)
+}
+
 //RawHoneypot
 func RawHoneypot(tmpl template.Template) {
 
 	r := mux.NewRouter()
+	var staticPath = "../public/honeypot/"
 
+	//registering endpoints
 	for _, request := range tmpl.Requests {
 		if request.Id != "default" {
 			request2 := request
-			r.HandleFunc(request2.Endpoint, func(w http.ResponseWriter, r *http.Request) {
-				genericWriter(w, r, request2.Content)
-			}).Methods(template.HttpMethodsAsString(request2.Methods)...)
+			if request2.ResponseType == "raw" {
+
+				r.HandleFunc(request2.Endpoint, func(w http.ResponseWriter, r *http.Request) {
+					w.Header().Add("Content-Type", request2.ContentType)
+					genericWriter(w, r, request2.Content)
+				}).Methods(template.HttpMethodsAsString(request2.Methods)...)
+
+			} else if request2.ResponseType == "file" {
+
+				r.HandleFunc(request2.Endpoint, func(w http.ResponseWriter, r *http.Request) {
+					w.Header().Add("Content-Type", request2.ContentType)
+					fileWriter(w, r, staticPath+request2.Content)
+				}).Methods(template.HttpMethodsAsString(request2.Methods)...)
+			}
 		}
 	}
-	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, template.DefaultResponse(tmpl))
-	})
+
+	//default response
+	defaultRequest := template.Default(tmpl)
+	if defaultRequest.ResponseType == "raw" {
+
+		r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("Content-Type", defaultRequest.ContentType)
+			genericWriter(w, r, defaultRequest.Content)
+		})
+
+	} else if defaultRequest.ResponseType == "file" {
+
+		r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("Content-Type", defaultRequest.ContentType)
+			fileWriter(w, r, staticPath+defaultRequest.Content)
+		})
+	}
 
 	srv := &http.Server{
 		Handler: r,
