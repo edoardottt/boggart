cd870091606fa015fbf59b091f90c7ef80a5cbe2
edoardottt <edoardott@gmail.com>
2022-04-12 18:16:53 +0200

    Add Log struct & InsertLog

--- db/log.go
+++ db/log.go
@@ -1,1 +1,26 @@
 package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+//Log defines the structure of a log record
+//in the database
+type Log struct {
+	Ip        string `json:"ip Str"`
+	Raw       string `json:"raw Str"`
+	Timestamp int64  `json:"timestamp Int"`
+}
+
+//InsertLog inserts a log record into the logs collection
+func InsertLog(client *mongo.Client, collection *mongo.Collection, record Log) {
+	result, err := collection.InsertOne(context.TODO(), record) //result
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted: ", result.InsertedID)
+}
