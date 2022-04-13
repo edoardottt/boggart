fc28060e21298d0f1e897ff189cd55a627770dd4
edoardottt <edoardott@gmail.com>
2022-04-13 18:46:43 +0200

    Init Api routes

--- api/routes.go
+++ api/routes.go
@@ -1,7 +1,8 @@
 package api
 
 const (
-	IpInfo     = "/api/{ip}"
-	LogsIp     = "/api/logs/{ip}"
-	LogsIpDate = "/api/logs/{ip}/{date}"
+	IpInfo      = "/api/{ip}"
+	LogsIp      = "/api/logs/{ip}"
+	LogsIpDate  = "/api/logs/{ip}/{date}"
+	LogsIpRange = "/api/logs/{ip}/{range}"
 )
