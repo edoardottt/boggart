3ecf6ba0e8fd07a230c5739718b800fb010fa471
edoardottt <edoardott@gmail.com>
2022-04-29 19:15:52 +0200

    Add LogsMethodDateHandler

--- api/api.go
+++ api/api.go
@@ -179,8 +179,40 @@ func LogsMethodHandler(w http.ResponseWriter, req *http.Request, dbName string, 
 //LogsMethodDateHandler >
 func LogsMethodDateHandler(w http.ResponseWriter, req *http.Request, dbName string, client *mongo.Client) {
 	w.Header().Add("Content-Type", "application/json")
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	database := db.GetDatabase(client, dbName)
+	collection := db.GetLogs(database)
 
-	fmt.Fprint(w, "TODO")
+	vars := mux.Vars(req)
+	date := vars["date"]
+	method := vars["method"]
+	dateT, err := time.Parse("2006-01-02", date)
+
+	// 400 BAD REQUEST: Time format != YYYY-MM-DD
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Cannot convert the date. Time format is YYYY-MM-DD.")
+		return
+	}
+	logs, err := db.GetLogsByMethodAndDate(client, collection, ctx, dateT, method)
+
+	// 500 INTERNAL SERVER ERROR: generic error
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Error while retrieving data.")
+		fmt.Println(err)
+		return
+	}
+
+	if len(logs) == 0 {
+		fmt.Fprintf(w, "{}")
+	} else {
+		err = json.NewEncoder(w).Encode(logs)
+		if err != nil {
+			fmt.Println(err) //DEBUG: logging!
+		}
+	}
 }
 
 //LogsMethodRangeHandler >
