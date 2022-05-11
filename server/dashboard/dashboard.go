d514e69fe3468189ac52f519706c3bf25f6516ad
edoardottt <edoardott@gmail.com>
2022-05-11 12:47:40 +0200

    Add Dashboard latest logs

--- server/dashboard/dashboard.go
+++ server/dashboard/dashboard.go
@@ -2,6 +2,7 @@ package dashboard
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -18,38 +19,50 @@ func Start() {
 
 	// DB setup
 	connString := os.Getenv("MONGO_CONN") // "mongodb://hostname:27017"
-	//dbName := os.Getenv("MONGO_DB")
+	dbName := os.Getenv("MONGO_DB")
 	client := db.ConnectDB(connString)
 	// ------- debug -------
 	if client != nil {
 		fmt.Println("DASHBOARD: Connected to MongoDB!")
 	}
 	baseTemplatePath := "./server/dashboard/templates/"
+	tmpl, err := template.ParseFiles(baseTemplatePath+"index.html",
+		baseTemplatePath+"navbar.html",
+		baseTemplatePath+"latest.html",
+		baseTemplatePath+"footer.html")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Routes setup
 	r := mux.NewRouter()
-	fs := http.FileServer(http.Dir("assets"))
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles(baseTemplatePath+"index.html",
-			baseTemplatePath+"navbar.html",
-			baseTemplatePath+"footer.html")
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		database := db.GetDatabase(client, dbName)
+		collection := db.GetLogs(database)
+		logs, err := db.GetLatestNLogs(client, collection, ctx, 30)
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		buf := &bytes.Buffer{}
-		err = tmpl.Execute(buf, nil)
+		err = tmpl.Execute(buf, logs)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		_, err = buf.WriteTo(w)
 		if err != nil {
+			log.Fatal(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	})
 
+	fs := http.FileServer(http.Dir("assets"))
 	r.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	srv := &http.Server{
